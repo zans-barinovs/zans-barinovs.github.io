5ef281b249cb7c3e7dc589bf94db372073779123
zans barinovs <zans.barinovs@gmail.com>
2020-04-11 21:24:30 +0300

    chainge photo and color grade this foto

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 height:100vh;background-image: -moz-linear-gradient(top, rgba(42,24,245,0.0), rgba(3,3,3,0.0));background-image: -webkit-linear-gradient(top, rgba(42,24,245,0.0), rgba(3,3,3,0.0));background-image: -o-linear-gradient(top, rgba(42,24,245,0.0), rgba(3,3,3,0.0));background-image: -ms-linear-gradient(top, rgba(42,24,245,0.0), rgba(3,3,3,0.0));background-image: linear-gradient(top, rgba(42,24,245,0.0), rgba(3,3,3,0.0));filter: progid:DXImageTransform.Microsoft.gradient(startColorStr='#fe2a18f5', endColorstr='#fe030303');
+
+
+t-cover__arrow-wrapper t-cover__arrow-wrapper_animated
